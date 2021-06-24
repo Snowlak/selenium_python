1b2074e55fde4cfea34f150a02634ba0ea1ad4d5
Snowlak <snezanaapolonik@gmail.com>
2021-06-24 23:59:35 +0300

    reload

--- venv/Include/уникальность селекторов(1.6 задание 11).py
+++ venv/Include/уникальность селекторов(1.6 задание 11).py
@@ -16,8 +16,6 @@ try:
     button = browser.find_element_by_css_selector('button.btn')
     button.click()
 
-    time.sleep(3)
-
     welcome_text_elt = browser.find_element_by_tag_name('h1')
     welcome_text = welcome_text_elt.text
 
